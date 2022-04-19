98d8c3f350e7599ec8c324dcbb45c4481d20e472
Philippos Vassiliou <PhilipposVassil@users.noreply.github.com>
2022-04-19 16:03:19 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+// enable CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // url of the frontend application
+    credentials: true // set credentials true for secure httpOnly cookie
+}));
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use cookie parser for secure httpOnly cookie
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+
+app.listen(port, () => {
+    console.log('Server started on: ' + port);
+});
